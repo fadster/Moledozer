ad75f41964b73744985af89bf110bf2a413f6cc6
fadster <fady69@gmail.com>
2015-09-18 03:54:56 -0400

    virtual method detour

---
+++ DetourTest/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Moledozer;
+
+namespace DetourTest
+{
+    public class TestClass
+    {
+        public virtual void TestMethod()
+        {
+            Console.Out.WriteLine("TestClass.TestMethod");
+        }
+    }
+
+    public class TestSubClass : TestClass
+    {
+    }
+
+    public class OtherSubClass : TestClass
+    {
+    }
+
+    public class DetourClass : TestClass
+    {
+        public void DetourMethod()
+        {
+            if ((this as object).GetType() != typeof(TestSubClass)) {
+                TestMethod();
+            } else {
+                Console.Out.WriteLine("Detour!");
+            }
+        }
+
+        public void DetourBaseMethod()
+        {
+            if ((this as object).GetType() != typeof(TestSubClass)) {
+                base.TestMethod();
+            } else {
+                Console.Out.WriteLine("Detour!");
+            }
+        }
+        /*
+        public new void TestMethod()
+        {
+            if ((this as object).GetType() != typeof(TestSubClass)) {
+                base.TestMethod();
+            } else {
+                Console.Out.WriteLine("Detour!");
+            }
+        }
+        */
+
+        public override void TestMethod()
+        {
+            if ((this as object).GetType() != typeof(TestSubClass)) {
+                Console.Out.WriteLine("TestClass.TestMethod");
+            } else {
+                Console.Out.WriteLine("Detour!");
+            }
+        }
+    }
+
+    public class DetourTest
+    {
+        static void Main()
+        {
+            TestClass obj = new TestClass();
+            TestSubClass subObj = new TestSubClass();
+            OtherSubClass otherObj = new OtherSubClass();
+
+            obj.TestMethod();
+            subObj.TestMethod();
+            otherObj.TestMethod();
+
+            try {
+                RedirectionHelper.RedirectCalls(typeof(TestSubClass).GetMethod("TestMethod"), typeof(DetourClass).GetMethod("TestMethod"));
+                obj.TestMethod();
+                subObj.TestMethod();
+                otherObj.TestMethod();
+            } catch (Exception e) {
+                Console.Out.WriteLine(e);
+            }
+        }
+    }
+}
